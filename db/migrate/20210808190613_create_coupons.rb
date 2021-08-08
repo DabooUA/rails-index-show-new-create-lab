52859da19ac3dd39a6e1450db2471061dc0b1f15
DabooUA <sbardysh@tutanota.com>
2021-08-08 16:13:24 -0400

    commit tree 3e398230e7d8eb012d021a75d8464842d9337361

--- db/migrate/20210808190613_create_coupons.rb
+++ db/migrate/20210808190613_create_coupons.rb
@@ -1,8 +1,10 @@
-class Coupons < ActiveRecord::Migration[5.0]
+class CreateCoupons < ActiveRecord::Migration[5.0]
   def change
     create_table :coupons do |c|
       c.string :coupon_code
       c.string :store
+
+      c.timestamps null: false
     end
   end
 end
